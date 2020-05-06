be6ca803f9540c64cbce7c7306daf7eefbeeebf5
ColtDahl <dahlcolt@gmail.com>
2020-05-06 13:26:21 -0500

    Add ViewAllListings page

---
+++ Community Gardens/Community Gardens/ViewAllListings.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Community_Gardens
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [System.ComponentModel.DesignTimeVisible(false)]
+    public partial class ViewAllListings : ContentPage
+    {
+        public ViewAllListings()
+        {
+        }
+
+        public void HandleClicks(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new Add_Listing();
+            Console.Out.WriteLine("CLICK");
+        }
+    }
+}
